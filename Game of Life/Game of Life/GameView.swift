b711b165b8f485b87e94d714cc25ca8baac462e4
Adrian Florescu <adrian.florescu@ro.ibm.com>
2016-04-27 18:43:14 +0300

    Added spacing

--- Game of Life/Game of Life/GameView.swift
+++ Game of Life/Game of Life/GameView.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class GameView: UIView {
+  private var lastUpdatedPoint: Point?
   let size: CGFloat = 10.0
   var game: Game?
 
@@ -16,44 +17,70 @@ class GameView: UIView {
   override func drawRect(rect: CGRect) {
     if let gU = game {
       for e in gU.entities {
-        drawEntity(e)
+        //drawEntity(e)
+        drawSquareEntity(e)
       }
-    } else {
-      let x = self.bounds.width / 2 - size / 2
-      let y = self.bounds.height / 2 - size / 2
-      let path = UIBezierPath(roundedRect: CGRectMake(x, y, size, size), cornerRadius: size / 10)
-      UIColor.blueColor().setFill()
-      path.fill()
-      drawEntity(Point(x: 0, y: 0))
-      drawEntity(Point(x: 2, y: 2))
     }
   }
 
   // MARK: Touches
   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    insertEntity(touches)
+    updateEntity(touches)
     self.setNeedsDisplay()
   }
 
   override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    insertEntity(touches)
+    updateEntity(touches)
     self.setNeedsDisplay()
   }
 
-  override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    insertEntity(touches)
-    self.setNeedsDisplay()
-  }
+//  override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//    updateEntity(touches)
+//    self.setNeedsDisplay()
+//  }
 
   func drawEntity(e: Point) {
     let path = UIBezierPath(roundedRect: CGRectMake(CGFloat(e.x) * size, CGFloat(e.y) * size, size, size), cornerRadius: size / 10)
     UIColor.blueColor().setFill()
     path.fill()
   }
+  
+  func drawSquareEntity(e: Point) {
+    let context = UIGraphicsGetCurrentContext()
+    let spaceing = size * 0.05
+    CGContextMoveToPoint   (context, CGFloat(e.x)     * size + spaceing, CGFloat(e.y)     * size + spaceing)
+    CGContextAddLineToPoint(context, CGFloat(e.x)     * size + spaceing, CGFloat(e.y + 1) * size - spaceing)
+    CGContextAddLineToPoint(context, CGFloat(e.x + 1) * size - spaceing, CGFloat(e.y + 1) * size - spaceing)
+    CGContextAddLineToPoint(context, CGFloat(e.x + 1) * size - spaceing, CGFloat(e.y)     * size + spaceing)
+    CGContextAddLineToPoint(context, CGFloat(e.x)     * size + spaceing, CGFloat(e.y)     * size + spaceing)
+    CGContextSetFillColorWithColor(context, UIColor.redColor().CGColor)
+    CGContextFillPath(context)
 
-  func insertEntity(touches: Set<UITouch>) {
+//    let context = UIGraphicsGetCurrentContext()
+//    CGContextSetLineWidth(context, 4.0)
+//    CGContextSetFillColor(context, UIColor.blueColor().CGColor)
+//    //CGContextSetStrokeColorWithColor(context, UIColor.blueColor().CGColor)
+//    let rectangle = CGRectMake(60,170,200,80)
+//    CGContextAddRect(context, rectangle)
+//    CGContextStrokePath(context)
+//    let path = CGRectMake(CGFloat(e.x) * size, CGFloat(e.y) * size, size, size)
+//    UIColor.blueColor().setFill()
+//    path.fill()
+  }
+
+  func updateEntity(touches: Set<UITouch>) {
     for t in touches {
-      game?.entities.insert(Point(x: Int(t.locationInView(self).x / size), y: Int(t.locationInView(self).y / size)))
+      let x = Int(t.locationInView(self).x / size)
+      let y = Int(t.locationInView(self).y / size)
+      let p = Point(x: x, y: y)
+      let gameU = game!
+//      if let lastUpdatedPointU = lastUpdatedPoint! {
+        if gameU.entities.contains(p) {
+          gameU.entities.remove(p)
+        } else {
+          gameU.entities.insert(p)
+        }
+//      }
     }
   }
 }
